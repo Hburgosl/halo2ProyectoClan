6a7b07834d57caee9cc4231fbe0436c701a4621e
AndyMuDz_Dev <andresrios508.ar@gmail.com>
2022-01-14 17:39:40 -0500

    cabecera color

--- templates/cabecera.php
+++ templates/cabecera.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/stilos.css?rand(1,2)">
+    <link rel="stylesheet" href="styles/estilos.css?rand(1,2)">
     <link rel="stylesheet" href="styles/webResponsiva.css?rand(1,2)">
     <title>Document</title>
 </head>
