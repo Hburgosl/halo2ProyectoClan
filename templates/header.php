890a8bbf3e48b32717df4b2df988f44f2f350854
AndyMuDz_Dev <andresrios508.ar@gmail.com>
2022-01-14 16:24:10 -0500

    header complete

---
+++ templates/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>|Dev|-clan</title>
+    <link rel="shortcut icon" href="" type="image/x-icon">
+</head>
+<body>
+    
+<div class="contenedor-head">
+
+<div class="contenido-head">
+    <img src="" alt="">
+    <h1>|DEV|-CLAN</h1>
+</div>
+
+
+
+</div>
