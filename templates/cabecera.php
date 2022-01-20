fa023a7899654006ae70a7c6c2c7c048832a633e
Hburgos_Dev <hburgosl@estudiante.uniajc.edu.co>
2022-01-20 17:02:12 -0500

    Se creó los estilos del footer

--- templates/cabecera.php
+++ templates/cabecera.php
@@ -7,12 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/estilos.css?rand(1,20)">
     <link rel="stylesheet" href="styles/webResponsiva.css?rand(1,20)">
+    <link rel="stylesheet" href="styles/estilosFooter.css?rand(1,20)">
     <title>|DEV|-CLAN</title>
 </head>
 
 <body>
 
-    <a href="#"><div class="contenedor-header">
-        
-        <h1>|DEV|-CLAN</h1>
-    </div></a>
+    <a href="#">
+        <div class="contenedor-header">
+
+            <h1>|DEV|-CLAN</h1>
+        </div>
+    </a>
