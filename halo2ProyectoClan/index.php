3c653bed52d960e7f0c850daa69a66de0b37d741
Hburgos_Dev <hburgosl@estudiante.uniajc.edu.co>
2022-01-20 16:12:02 -0500

    hola

---
+++ halo2ProyectoClan/index.php
@@ -0,0 +1,17 @@
+<?php include 'templates/cabecera.php' ?>
+
+<div class="contenedor-contenido">
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+<?php include 'templates/pieDePagina.php'?>
---
+++ halo2ProyectoClan/templates/pieDePagina.php
@@ -0,0 +1,14 @@
+       <footer>
+
+           <div class="contenedor-redes">
+               <span><i class="fab fa-facebook-square"></i></span>
+               <span><i class="fab fa-instagram"></i></span>
+               <span><i class="fab fa-twitter"></i></span>
+           </div>
+
+       </footer>
+
+       <script src="https://kit.fontawesome.com/f1dcbdae79.js" crossorigin="anonymous"></script>
+       </body>
+
+       </html>
